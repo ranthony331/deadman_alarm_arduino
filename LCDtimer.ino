ae3b1e6f35076383c7693824f084ff83d2a7e858
ranthony331 <49055727+ranthony331@users.noreply.github.com>
2021-11-23 20:28:49 +0800

    Beta version no BT interrupt
    Stable version without bluetooth interrupt

--- LCDtimer.ino
+++ LCDtimer.ino
@@ -13,10 +13,11 @@ const int timer_alive = 1;
 const int levelOne = 3;
 const int levelTwo = 4;
 const int levelThree =5;
-
+int alarm_level = -1;
 int hrs = 0;
 int Min = 0;
 int sec = 0;
+int timer_gap = 0; //TIMER GAP FOR HIGHER LEVEL
 int warning_time = 3;
 unsigned int check_val = 50;
 int add_chk = 0;
@@ -39,6 +40,7 @@ unsigned long currentMillis_reset = 0;
 
 void setup()
 { 
+  Serial.begin(9600);
   Serial.print("PROG START\n");
   lcd.begin(16, 2);
   lcd.clear();
@@ -72,7 +74,7 @@ void setup()
     Min = EEPROM.read(add_min);
     sec = EEPROM.read(add_sec);
   }
-  delay(1500);
+  delay(500);
   INIT();
 }
 
@@ -82,6 +84,7 @@ void loop()
   {
     lcd.clear();
     lcd.setCursor(2, 0);
+    Serial.println("Start button pressed");
     lcd.print("ALARM TIMER"); // monitor milllis
     RUN = true;
     while (RUN)
@@ -89,38 +92,212 @@ void loop()
       
       if(digitalRead(reset) == LOW){
         currentMillis_reset = millis();
+        lcd.clear();
+        lcd.setCursor(2,0);
+        lcd.print("ALARM TIMER");
         RESET();
         }
       currentMillis = millis();
       digitalWrite(timer_alive, HIGH);
       
-      if((currentMillis - previousMillis) > interval) {
-        previousMillis = currentMillis;   
-        sec = sec - 1;
-      }
       if(sec <= warning_time){
-        //BUZZER_ALARM(levelOne);
+        lcd.setCursor(1,0);
+        lcd.print("WARNING: RESET");
+        digitalWrite(buzz, HIGH);
+        digitalWrite(preAlarm, HIGH);
+        alarm_level = 0;     
         }
        TIMER_DECREMENT_PRINT();
       if (hrs == 0 && Min == 0 && sec == 0){
+        Serial.println("inside level 1 if");
+        digitalWrite(preAlarm, HIGH);
+        digitalWrite(levelOne, HIGH);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("  LEVEL 1 ALARM");
+        alarm_level = 1;
+        lcd.setCursor(0,1);
+        Serial.println("alarm level value: " + String(alarm_level));
+        RESET_TIME();
+        while( (alarm_level==1) && (digitalRead(reset)==HIGH) ){
+           TIMER_DECREMENT_PRINT();
+           if (hrs == 0 && Min == 0 && sec == 0){
+               Serial.println("inside level 2 if");
+              digitalWrite(preAlarm, HIGH);
+              digitalWrite(levelOne, HIGH);
+              digitalWrite(levelTwo, HIGH);
+              alarm_level = 2;
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("  LEVEL 2 ALARM");
+              Serial.println("alarm level value: " + String(alarm_level));
+              RESET_TIME();
+              while( (alarm_level==2) && (digitalRead(reset)==HIGH) ){
+                
+                TIMER_DECREMENT_PRINT();
+                if (hrs == 0 && Min == 0 && sec == 0){
+                  Serial.println("inside level 3 if");
+                  digitalWrite(preAlarm, HIGH);
+                  digitalWrite(levelOne, HIGH);
+                  digitalWrite(levelTwo, HIGH);
+                  digitalWrite(levelThree, HIGH);
+                  lcd.clear();
+                  lcd.setCursor(0,0);
+                  lcd.print("  LEVEL 3 ALARM");
+                  Serial.println("alarm level value: " + String(alarm_level));
+                  alarm_level = 3;
+                  RESET_TIME();
+                  while((alarm_level==3) && (digitalRead(reset)==HIGH)){
+                    TIMER_DECREMENT_PRINT();
+                    if (hrs == 0 && Min == 0 && sec == 0){
+                      lcd.setCursor(0,1);
+                      lcd.print("   DEAD ALARM  ");
+                      Serial.println("inside level final stage IF");
+                      while(digitalRead(reset)==HIGH){
+                        //PROGRAM LOOP STUCK WHILE NOT RESET
+                        }//while RESET is still not pressed after 3 levels
+                      }//if level 3 time expired
+                    }//while alarm_level == 3
+                  }//if level 2 time expired
+                  
+                }//while alarm_level == 2
+            }//if level 1 time expired
+           
+          }//while alarm_level == 1
+        
         digitalWrite(timer_alive, LOW);
         lcd.setCursor(4, 0);
         RUN = false;
-        for (int i = 0; i < 5; i++){
-          deadAlarm = true;
-          while(deadAlarm){
-            BUZZER_ALARM(levelOne);        
-            if(digitalRead(reset) == LOW){
-              deadAlarm=false;
-              digitalWrite(buzz, LOW);
-              digitalWrite(preAlarm, LOW);
-             }  
-          }
-         }
+
+         
         INIT();
       }//if timer is at 0:0:0
     }//while TIMER RUN=true
   }//end of if START BUTTON PRESSED
+  EDIT_TIME();
+
+  if(digitalRead(reset)==LOW){
+    RESET();
+    }
+}
+
+void RESET_TIME_HIGHER_LEVEL(){
+  currentMillis = millis();
+  hrs = EEPROM.read(add_hrs) - timer_gap;
+  Min = EEPROM.read(add_min) - timer_gap;
+  sec = EEPROM.read(add_sec) - timer_gap;
+  }
+void RESET_TIME(){
+  currentMillis = millis();
+  hrs = EEPROM.read(add_hrs);
+  Min = EEPROM.read(add_min);
+  sec = EEPROM.read(add_sec);
+  }
+
+void RESET()
+{
+  currentMillis = millis();
+  hrs = EEPROM.read(add_hrs);
+  Min = EEPROM.read(add_min);
+  sec = EEPROM.read(add_sec);
+
+  if(alarm_level==0){
+    digitalWrite(preAlarm, LOW);
+    digitalWrite(buzz, LOW);
+    alarm_level=-1;
+    }
+  if(alarm_level==1){
+    digitalWrite(preAlarm, LOW);
+    digitalWrite(levelOne, LOW);
+    digitalWrite(buzz, LOW);
+    alarm_level=-1;
+    }
+  if(alarm_level==2){
+    digitalWrite(preAlarm, LOW);
+    digitalWrite(levelOne, LOW);
+    digitalWrite(levelTwo, LOW);
+    digitalWrite(buzz, LOW);
+    alarm_level=-1;
+    }
+  if(alarm_level==3){
+    digitalWrite(preAlarm, LOW);
+    digitalWrite(levelOne, LOW);
+    digitalWrite(levelTwo, LOW);
+    digitalWrite(levelThree, LOW);
+    digitalWrite(buzz, LOW);
+    alarm_level=-1;
+    }
+
+   Serial.println("Resetting with alarm level: " + String(alarm_level));
+}
+
+
+void TIMER_DECREMENT_PRINT(){
+        currentMillis = millis();
+        if((currentMillis - previousMillis) > interval) {
+        previousMillis = currentMillis;   
+        sec = sec - 1;
+        }
+        if (sec == -1){
+        sec = 59;
+        Min = Min - 1;
+         }
+      if (Min == -1){
+          Min = 59;
+          hrs = hrs - 1;
+        }
+      if (hrs == -1) hrs = 0;
+      lcd.setCursor(4, 1);
+      if (hrs <= 9){
+        lcd.print('0');
+        }
+      lcd.print(hrs);
+      lcd.print(':');
+      if (Min <= 9){
+        lcd.print('0');
+        }
+      lcd.print(Min);
+      lcd.print(':');
+      if (sec <= 9){
+        lcd.print('0');
+       }
+      lcd.print(sec);
+}
+void INIT()
+{
+  hrs = EEPROM.read(add_hrs);
+  Min = EEPROM.read(add_min);
+  sec = EEPROM.read(add_sec);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Start Countdown");
+  lcd.setCursor(4, 1);
+  if (hrs <= 9)
+  {
+    lcd.print('0');
+  }
+  lcd.print(hrs);
+  lcd.print(':');
+  if (Min <= 9)
+  {
+    lcd.print('0');
+  }
+  lcd.print(Min);
+  lcd.print(':');
+  if (sec <= 9)
+  {
+    lcd.print('0');
+  }
+  lcd.print(sec);
+  sec_flag = true;
+  min_flag = true;
+  hrs_flag = true;
+  //delay(500);
+}
+
+
+
+void EDIT_TIME(){
   if ((digitalRead(set) == LOW) & RUN==false)
   {
     delay(500);
@@ -214,86 +391,4 @@ void loop()
     }
     INIT();
   }
-}
-
-void RESET()
-{
-  currentMillis_reset = millis();
-  hrs = EEPROM.read(add_hrs);
-  Min = EEPROM.read(add_min);
-  sec = EEPROM.read(add_sec);
-
-}
-
-void BUZZER_ALARM(const int indicator){
-currentMillis = millis();
-  if((currentMillis - previousMillis) > buzz_interval){
-     previousMillis = currentMillis;   
-     digitalWrite(buzz, !(digitalRead(buzz)));
-     digitalWrite(indicator, HIGH);
-     }  
-}
-
-
-
-
-
-
-void TIMER_DECREMENT_PRINT(){
-        if (sec == -1){
-        sec = 59;
-        Min = Min - 1;
-       }
-      if (Min == -1){
-        Min = 59;
-        hrs = hrs - 1;
-       }
-      if (hrs == -1) hrs = 0;
-      lcd.setCursor(4, 1);
-      if (hrs <= 9){
-        lcd.print('0');
-        }
-      lcd.print(hrs);
-      lcd.print(':');
-      if (Min <= 9){
-        lcd.print('0');
-        }
-      lcd.print(Min);
-      lcd.print(':');
-      if (sec <= 9){
-        lcd.print('0');
-       }
-      lcd.print(sec);
-  }
-
-void INIT()
-{
-  hrs = EEPROM.read(add_hrs);
-  Min = EEPROM.read(add_min);
-  sec = EEPROM.read(add_sec);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Start Countdown");
-  lcd.setCursor(4, 1);
-  if (hrs <= 9)
-  {
-    lcd.print('0');
-  }
-  lcd.print(hrs);
-  lcd.print(':');
-  if (Min <= 9)
-  {
-    lcd.print('0');
-  }
-  lcd.print(Min);
-  lcd.print(':');
-  if (sec <= 9)
-  {
-    lcd.print('0');
-  }
-  lcd.print(sec);
-  sec_flag = true;
-  min_flag = true;
-  hrs_flag = true;
-  delay(500);
 }
